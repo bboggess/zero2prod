1d52b6bc97cf241358a5426c79ce962618fa799a
bboggess3 <b.boggess727@gmail.com>
2023-09-25 20:16:18 -0500

    Switch to using subscriber based tracing
    Move from naive logging to tracing with the tracing crate.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::{EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let format_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(format_layer);
+    set_global_default(subscriber).expect("Failed to set tracing subscriber");
 
     let configuration = get_configuration().expect("Failed to read configuration");
 
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{post, web, HttpResponse, Responder};
 use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 /// The data being submitted from the subscription form
@@ -14,28 +15,41 @@ pub struct FormData {
 /// Adds a new subscription.
 #[post("/subscribe")]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
-    log::info!("Saving new subscriber details in the database");
+    let email = &form.email;
+    let name = &form.name;
+    let request_id = Uuid::new_v4();
 
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %email,
+        subscriber_name = %name
+    );
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving subscriber details in database.");
     let query_result = sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        email,
+        name,
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await;
 
     match query_result {
-        Ok(_) => {
-            log::info!("New subscriber details have been saved");
-            HttpResponse::Ok()
-        }
+        Ok(_) => HttpResponse::Ok(),
         Err(err) => {
-            log::error!("Failed to execute query: {:?}", err);
+            tracing::error!(
+                "Request ID {}: Failed to execute query: {:?}",
+                request_id,
+                err
+            );
             HttpResponse::InternalServerError()
         }
     }
