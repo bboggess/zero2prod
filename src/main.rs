d3ec0118e6984a2fa33cdfa712043e05e3c4a4fd
bboggess3 <b.boggess727@gmail.com>
2023-10-24 20:36:52 -0500

    Refactor behavior out of main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-use sqlx::PgPool;
-use std::net::TcpListener;
-use url::Url;
 use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::EmailClient;
-use zero2prod::startup::run;
+use zero2prod::startup;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[actix_web::main]
@@ -12,23 +8,8 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
+    let server = startup::build(configuration).await?;
+    server.await?;
 
-    let connection_pool = PgPool::connect_lazy_with(configuration.database.database_connection());
-
-    let app_config = configuration.application;
-    let app_address = format!("{}:{}", &app_config.host, app_config.port);
-    let listener = TcpListener::bind(app_address)?;
-
-    let email_config = configuration.email_client;
-    let base_url = Url::parse(&email_config.base_url).expect("Invalid base URL");
-    let sender_email = email_config.sender().expect("Invalid sender email address");
-    let timeout = email_config.timeout();
-    let email_client = EmailClient::new(
-        base_url,
-        sender_email,
-        email_config.authorization_token,
-        timeout,
-    );
-
-    run(listener, connection_pool, email_client)?.await
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -3,13 +3,38 @@ use std::net::TcpListener;
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
+use url::Url;
 
 use crate::{
+    configuration::Settings,
     email_client::EmailClient,
     routes::{health_check, subscribe},
 };
 
-/// Builds an HttpServer running our app at the specified address
+/// Build an HTTP server running our app. The behavior of the app is configured
+/// through the `settings` argument.
+pub async fn build(settings: Settings) -> std::io::Result<Server> {
+    let connection_pool = PgPool::connect_lazy_with(settings.database.database_connection());
+
+    let email_config = settings.email_client;
+    let base_url = Url::parse(&email_config.base_url).expect("Invalid base URL");
+    let sender_email = email_config.sender().expect("Invalid sender email address");
+    let timeout = email_config.timeout();
+    let email_client = EmailClient::new(
+        base_url,
+        sender_email,
+        email_config.authorization_token,
+        timeout,
+    );
+
+    let app_config = settings.application;
+    let app_address = format!("{}:{}", &app_config.host, app_config.port);
+    let listener = TcpListener::bind(app_address)?;
+
+    run(listener, connection_pool, email_client)
+}
+
+/// Starts a server, listening on `listener`, running in the background and returns it
 pub fn run(
     listener: TcpListener,
     db_pool: PgPool,
